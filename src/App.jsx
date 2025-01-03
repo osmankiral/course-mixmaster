07c570be22e7d9bd265efc64ff10727cae5b7a45
osmankiral <osman.kiral96@gmail.com>
2025-01-03 23:22:02 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  About,
+  Cocktail,
+  Error,
+  HomeLayout,
+  Landing,
+  Newsletter,
+} from "./pages";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout/>,
+  },
+  {
+    path: "/about",
+    element: <About/>,
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+export default App;
