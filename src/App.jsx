2fe3e1c5b0847cc6ed29216d188a2af68238aff9
osmankiral <osman.kiral96@gmail.com>
2025-01-05 23:49:48 +0300

    nested pages

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,26 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <HomeLayout/>,
+    children: [
+      {
+        index: true,
+        element: <Landing/>,
+      },
+      {
+        path: "cocktail",
+        element: <Cocktail/>,
+      },
+      {
+        path: "newsletter",
+        element: <Newsletter/>,
+      },
+      {
+        path: "about",
+        element: <About/>,
+      },
+    ]
   },
-  {
-    path: "/about",
-    element: <About/>,
-  },
+  
 ]);
 
 const App = () => {
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div>
       <h1>About</h1>
-      <Link to='/'>Home Pageee</Link>
+      <Link to='/'>Home Page</Link>
     </div>
   );
 };
